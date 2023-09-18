021f45027c435847f9e8dd18c983200517e43852
Ronald Otieno <ronnlats@gmail.com>
2023-09-18 18:43:21 +0300

    Add login logic

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+let password,email;
+
+const { data, error } = await supabase.auth.signInWithPassword({
+  email: email,
+  password: password,
+})
+</script>
+
 <div class="max-w-[280px] mx-auto">
     <div class="flex flex-col items-center mt-[10vh]">
         <h2 class="mb-5 text-gray-900 font-mono font-bold text-xl">Login</h2>
@@ -68,12 +77,16 @@
                 class="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium"
                 placeholder="Email"
                 value=""
+
+bind:this={email}
+
             />
             <input
                 type="password"
                 class="w-full px-6 py-3 mb-2 border border-slate-600 rounded-lg font-medium"
                 placeholder="Password"
                 value=""
+bind:this={password}
             />
             <button
                 class="bg-slate-500 hover:bg-slate-700 text-white text-base rounded-lg py-2.5 px-5 transition-colors w-full text-[19px]"
